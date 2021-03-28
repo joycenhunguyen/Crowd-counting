727b5a56827c403bc61d05a7abdc53c54e096a7b
Thuong <Thuong89@users.noreply.github.com>
2021-03-28 15:31:52 +0300

    instruction part completed & relayout responsive

--- first_app.py
+++ first_app.py
@@ -13,7 +13,7 @@ def load_image(image_file):
 # Set page config
 apptitle = 'Crowd Counting'
 
-st.set_page_config(page_title=apptitle, page_icon=":fox_face:")
+st.set_page_config(page_title=apptitle, page_icon=":fox_face:",layout='wide')
 
 #Save upload file
 def save_uploadedfile(image_file, part_name):
@@ -24,8 +24,41 @@ def save_uploadedfile(image_file, part_name):
 header = st.beta_container()
 
 with header:
-  st.title('Crowd Counting App')
-  st.text('In this project, we use....')
+  st.markdown(
+    """<style>     .container {display: flex;flex-wrap:wrap;}</style>""",unsafe_allow_html=True)
+  st.markdown("""<h1>Crowd Counting App</h1>
+  <hr>
+  <h2>Instruction </h2>
+  <p>This app is designed to count a crowd which is not easily counted and depends on how crowded is in the photo. We give examples of bad images 
+  (because they are not crowded and do not need to run the app to count), and examples of Dense vs Non-dense imanges for your references. Dense images are impossible to count manually, and non-dense images are somewhat manageable to count with effort.</p>
+  <p class='container'>
+  <span>
+  <img src="https://bambiniphoto.sg/wp-content/uploads/family-photography-bambini-025.jpg" alt="avoid1" style="width:300px;height:150px;padding:5px">
+  <figcaption style='text-align:center;color:red'>Not good</figcaption>
+  </span>
+  <span>
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRj42KnQQ3y_upUSOSYC_dC33tSUxsj1ck2iQ&usqp=CAU" alt="avoid2" style="width:200px;height:150px;padding:5px">
+  <figcaption style='text-align:center;color:red'>Not good</figcaption>
+  </span>
+  <span>
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4PqTHWIxkQnoWIwp4jHhizRRzNRuhaKQS-MsEL5P82OUbfam2sLcURc_WvsNYk1vlGs8&usqp=CAU" alt="avoid1" style="width:200px;height:150px;padding:5px">
+  <figcaption style='text-align:center;color:green'>Dense</figcaption>
+  </span>
+  <span>
+  <img src="https://ak.picdn.net/shutterstock/videos/4974800/thumb/1.jpg" alt="avoid2" style="width:200px;height:150px;padding:5px">
+  <figcaption style='text-align:center;color:green'>Dense</figcaption>
+  </span>
+    <span>
+  <img src="https://as.cornell.edu/sites/default/files/styles/4_5/public/field/image/crowd450.jpg?itok=ppSyCQT7" alt="good1" style="width:200px;height:150px;padding:5px">
+  <figcaption style='text-align:center;color:blue'>Non-dense</figcaption>
+  </span>
+  <span>
+  <img src="http://marybaldwin.edu/wp-content/uploads/2018/11/MDCHS-1024x343.jpg" alt="good2" style="width:200px;height:150px;padding:5px">
+  <figcaption style='text-align:center;color:blue'>Non-dense</figcaption>
+  </span>
+  </p>
+  <hr>
+  """, unsafe_allow_html=True)
 #Sidebar
 
 st.sidebar.title('Select your image')
@@ -37,25 +70,35 @@ def main():
   choice = st.sidebar.selectbox('Options', options = menu)
 
   if choice == 'Dense Image':
-    st.subheader (' High Density Crowd Image')
+    st.markdown('## Result')
     image_file = st.sidebar.file_uploader(' ', type= ['jpeg', 'png', 'jpg'], key = choice)
     
     if image_file is not None:
-      st.image(load_image(image_file), width = 500, caption = 'Uploaded image')
-      save_uploadedfile(image_file, 'A')
-      image_heat=engine.run(image_file,'A')
-      st.image(image_heat,width = 500, caption = 'heatmap image')
-      st.write('Predicted number of people:', round(np.sum(image_heat)))
+      col1, col2,col3 = st.beta_columns(3)
+      with col1:
+        st.image(load_image(image_file), width = 500, caption = 'Uploaded image')
+        save_uploadedfile(image_file, 'A')
+      with col2:
+        image_heat=engine.run(image_file,'A')  
+        st.image(image_heat,width = 500, caption = 'heatmap image')
+      with col3: 
+        st.write('Predicted number of people:', round(np.sum(image_heat)))
+  
                  
   else:
-    st.subheader('Low Density Crowd Image')
+    st.markdown('## Result')
     image_file = st.sidebar.file_uploader(' ', type= ['jpeg', 'png', 'jpg'], key = choice)
     if image_file is not None:
-      st.image(load_image(image_file), width = 500)
-      save_uploadedfile(image_file, 'B')
-      image_heat=engine.run(image_file,'B')
-      st.image(image_heat,width = 500, caption = 'heatmap image')
-      st.write('Predicted number of people:', round(np.sum(image_heat)))    
+      col1, col2,col3 = st.beta_columns(3)
+      with col1:
+        st.image(load_image(image_file), width = 500, caption = 'Uploaded image')
+        save_uploadedfile(image_file, 'B')
+      with col2:
+        image_heat=engine.run(image_file,'B')
+        st.image(image_heat,width = 500, caption = 'heatmap image')
+      with col3:
+        st.write('Predicted number of people:', round(np.sum(image_heat))) 
+ 
 
 if __name__ == '__main__':
     main()
@@ -63,5 +106,5 @@ if __name__ == '__main__':
 
 st.subheader("About this app")
 st.markdown("""
-This app displays data....
+This app is developed by Nhu Nguyen, Thuong Nguyen, Radim Musalek basing on the previously well developed models (details in our github link).
 """)
